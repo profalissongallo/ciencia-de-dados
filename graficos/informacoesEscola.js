ec12b14a9e623ca8c097a297ec52ae0396e36a36
profalissongallo <158321480+profalissongallo@users.noreply.github.com>
2024-10-21 22:37:34 -0300

    Add files via upload

---
+++ graficos/informacoesEscola.js
@@ -0,0 +1,20 @@
+const url = 'graficos/total_pessoas.json'
+
+async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    const pessoasConectadas = dados.total_pessoas_conectadas
+    const pessoasNaEscola = dados.total_pessoas_escola
+    const horas = parseInt(dados.tempo_medio)
+    const minutos = Math.round((dados.tempo_medio - horas) * 100)
+    const porcentagemConectada = ((pessoasConectadas / pessoasNaEscola ) * 100).toFixed(2)
+
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que a escola tem <span>${pessoasNaEscola}</span> pessoas e que aproximadamente <span>${pessoasConectadas}</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social.`
+
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
+}
+
+vizualizarInformacoesGlobais()
